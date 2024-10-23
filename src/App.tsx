d5456134aece50c98bddc8fa52c6b627a003f237
Dmytro <dmytrostoianov05@gmail.com>
2024-10-23 15:58:31 +0200

    Initial commit
    Initialize project with Vite, React, TypeScript, TailwindCSS, ESLint.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <>
+            <h1 className="text-[1rem] font-bold text-red-500 underline">
+                testing tailwind
+            </h1>
+        </>
+    )
+}
+
+export default App
